5159389b2470f444048fbd05a36ab7b0fac74078
Katarzyna <125917863+KhiRRra@users.noreply.github.com>
2023-05-09 20:28:48 +0100

    Add files

---
+++ AnimBg.js
@@ -0,0 +1,20 @@
+class AnimBg {
+  constructor(selector) {
+    this.elements = document.querySelectorAll(selector);
+  }
+
+  listenCursorMove(e) {
+    const { clientX, clientY } = e;
+    const centerX = window.innerWidth / 2;
+    const centerY = window.innerHeight / 2;
+
+    this.elements.forEach((element) => {
+      const ratioX = -element.getAttribute("ratioX");
+      const ratioY = -element.getAttribute("ratioY");
+
+      const moveX = clientX - centerX;
+      const moveY = clientY - centerY;
+      element.style.transform = `translate(${moveX * ratioX}px, ${moveY * ratioY}px)`;
+    });
+  };
+}
